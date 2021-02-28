15901587a8991ea2cd885edf441055890145c4ab
KristaYanb <yanbeckova.kristina@yandex.ru>
2021-02-28 15:16:46 +0700

    new functions for output in file

--- square_In_Out.cpp
+++ square_In_Out.cpp
@@ -1,4 +1,6 @@
 #include <fstream>
+#include <istream>
+#include <string>
 #include "square_atd.h"
 using namespace std;
 
@@ -7,14 +9,16 @@ namespace simple_matrix
 	void In(square& r, ifstream& ifst) // Ввод параметров квадратной матрицы из файла
 	{
 		ifst >> r.size;
+
 		for (int i = 1; i <= r.size; i++)
 			for (int j = 1; j <= r.size; j++)
 				ifst >> r.mas_square[i][j];
 	}
 
-	void Out(square& r, ofstream& ofst) // Вывод параметров квадратной матрицы в файл
+	void Out1(square& r, ofstream& ofst) // Вывод параметров квадратной матрицы в файл
 	{
 		ofst << "It is Square matrix: Size = " << r.size << endl;
+
 		for (int i = 1; i <= r.size; i++)
 			for (int j = 1; j <= r.size; j++)
 			{
@@ -22,5 +26,30 @@ namespace simple_matrix
 				if (j == r.size)
 					ofst << endl;
 			}
+	}
+
+	void Out2(square& r, ofstream& ofst) // Вывод параметров квадратной матрицы в файл
+	{
+		ofst << "It is Square matrix: Size = " << r.size << endl;
+		
+		for (int j = 1; j <= r.size; j++)
+			for (int i = 1; i <= r.size; i++)
+			{
+				ofst << r.mas_square[i][j] << " ";
+				if (i == r.size)
+					ofst << endl;
+			}
+	}
+
+	void Out3(square& r, ofstream& ofst) // Вывод параметров квадратной матрицы в файл
+	{
+		ofst << "It is Square matrix: Size = " << r.size << endl;
+		
+		for (int i = 1; i <= r.size; i++)
+			for (int j = 1; j <= r.size; j++)
+			{
+				ofst << r.mas_square[i][j] << " ";
+			}
+		ofst << endl;
 	}
 } // end simple_matrix namespace
