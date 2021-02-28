0a153f497b752c7111a7d67ec02ed68373a32f8c
KristaYanb <yanbeckova.kristina@yandex.ru>
2021-02-28 15:02:34 +0700

    new key and object

--- matr_atd.h
+++ matr_atd.h
@@ -3,19 +3,21 @@
 #define __matr_atd__
 #include "square_atd.h"
 #include "diag_atd.h"
+#include "nizn_diag_atd.h"
 
 namespace simple_matrix
 {
 	struct matr // структура, обобщающая все матрицы
 	{
 		int size;
-		enum key { SQUARE, DIAG }; // значения ключей для каждой из матриц
+		enum key { SQUARE, DIAG, NIZN_DIAG }; // значения ключей для каждой из матриц
 		key k; // ключ
 
 		union  // используемые альтернативы
 		{ // используем включение
 			square s;
 			diag d;
+			nizn_diag n;
 		};
 	};
 } // end simple_matrix namespace
