e0f71989a6500c8abac1a610600db9386998d48f
KristaYanb <yanbeckova.kristina@yandex.ru>
2021-02-28 15:03:15 +0700

    first commit

---
+++ nizn_diag_atd.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef __nizn_diag_atd__
+#define __nizn_diag_atd__
+
+namespace simple_matrix
+{
+	struct nizn_diag // квадратная матрица
+	{
+		int size; // размерность
+		int mas_nizn_diag[50]; //одномерный массив
+	};
+} // end simple_matrix namespace
+#endif
